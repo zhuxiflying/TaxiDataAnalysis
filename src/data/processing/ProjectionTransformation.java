745729e88b1afb9fc29b64f7d4fefa2b618b04fb
zhuxi <zhuxi@DESKTOP-TRH9AU5>
2018-09-26 16:05:29 -1000

    partition the data file into hour duration.

--- src/data/processing/ProjectionTransformation.java
+++ src/data/processing/ProjectionTransformation.java
@@ -3,72 +3,63 @@ package data.processing;
 import java.io.FileReader;
 import java.io.FileWriter;
 
-
 import org.osgeo.proj4j.CRSFactory;
 import org.osgeo.proj4j.CoordinateReferenceSystem;
 import org.osgeo.proj4j.CoordinateTransform;
 import org.osgeo.proj4j.CoordinateTransformFactory;
 import org.osgeo.proj4j.ProjCoordinate;
 
-
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 
-
-/** 
- * The ProjectionTransformation class implements an coordinates projection by Proj4j.
- * Specifically, the example below demonstrate the projection from WGS84 to UTM18N.
- * The New York Taxi Trip data (yellow cab trips from 2009 to 2015) adopted in this example.
+/**
+ * The ProjectionTransformation class implements an coordinates projection by
+ * Proj4j. Specifically, the example below demonstrate the projection from WGS84
+ * to UTM18N. The New York Taxi Trip data (yellow cab trips from 2009 to 2015)
+ * adopted in this example.
  */
 public class ProjectionTransformation {
-	
-	
-	public static void main(String[] args) throws Exception
-	{
-		
+
+	public static void main(String[] args) throws Exception {
+
 		CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
-	  	CRSFactory csFactory = new CRSFactory();
-	  	
-	  	//create input WGS84
-	  	final String WGS84_PARAM = "+title=long/lat:WGS84 +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
-	    CoordinateReferenceSystem WGS84 = csFactory.createFromParameters("WGS84",WGS84_PARAM);
+		CRSFactory csFactory = new CRSFactory();
 
-	    //create output UTM18N
-	    CoordinateReferenceSystem UTM18N = csFactory.createFromName("EPSG:32618");
-	    
-	    //create transformer
-	    CoordinateTransform trans = ctFactory.createTransform(WGS84, UTM18N);
-	  	
-	    ProjCoordinate p1 = new ProjCoordinate();
-	    ProjCoordinate p2 = new ProjCoordinate();
+		// create input WGS84
+		final String WGS84_PARAM = "+title=long/lat:WGS84 +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees";
+		CoordinateReferenceSystem WGS84 = csFactory.createFromParameters("WGS84", WGS84_PARAM);
 
-	    
-	    /*
-	     * Transform point p1 to p2;
-	     * 
-	     * double[] coordinates = {-73.995917,40.728576};
-	     *  p1.x = coordinates[0];
-	     *  p1.y = coordinates[1];
-	     *  trans.transform(p1, p2);
-	     *  System.out.println(p2.x);
-	     *  System.out.println(p2.y;);
-	     */
-	    
-	    
-	    
-	    //traversal the file list.
-		String[] years = { "2009", "2010", "2011", "2012", "2013", "2014", "2015" };
-		String[] months = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
+		// create output UTM18N
+		CoordinateReferenceSystem UTM18N = csFactory.createFromName("EPSG:32618");
 
+		// create transformer
+		CoordinateTransform trans = ctFactory.createTransform(WGS84, UTM18N);
+
+		ProjCoordinate p1 = new ProjCoordinate();
+		ProjCoordinate p2 = new ProjCoordinate();
+
+		/*
+		 * Transform point p1 to p2;
+		 * 
+		 * double[] coordinates = {-73.995917,40.728576}; p1.x = coordinates[0]; p1.y =
+		 * coordinates[1]; trans.transform(p1, p2); System.out.println(p2.x);
+		 * System.out.println(p2.y;);
+		 */
+
+		// traversal the file list.
+//		String[] years = { "2009", "2010", "2011", "2012", "2013", "2014", "2015" };
+//		String[] months = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
+		String[] years = { "2009" };
+		String[] months = { "03" };
 		for (int y = 0; y < years.length; y++) {
 			for (int m = 0; m < months.length; m++) {
 				String year = years[y];
 				String month = months[m];
-				String filename = "D:\\Data\\NY_Taxi_tripdata\\yellow_tripdata_"+year+"-"+month+".csv";
+				String filename = "D:\\Data\\yellow_tripdata_" + year + "-" + month + ".csv";
 //				System.out.println(filename);
-				String filename2 = "D:\\Data\\NY_Taxi_tripdata2\\yellow_tripdata_"+year+"-"+month+".csv";
+				String filename2 = "D:\\Data\\NY_Taxi_tripdata\\yellow_tripdata_" + year + "-" + month + ".csv";
 				CSVReader reader = new CSVReader(new FileReader(filename));
-				
+				reader.readNext();
 				String[] nextLine;
 
 				CSVWriter writer = new CSVWriter(new FileWriter(filename2), ',', CSVWriter.NO_QUOTE_CHARACTER);
@@ -86,45 +77,42 @@ public class ProjectionTransformation {
 				writer.writeNext(entries);
 
 				while ((nextLine = reader.readNext()) != null) {
-					
+					try {
 						String[] dataitem = new String[7];
-						
-						//read pickup date, drop off date, trip distance ,and write into new file.
+
+						// read pickup date, drop off date, trip distance ,and write into new file.
 						dataitem[0] = nextLine[1];
 						dataitem[1] = nextLine[2];
 						dataitem[2] = nextLine[4];
-						
-					    //read WGS coordinates from the file.
+
+						// read WGS coordinates from the file.
 						double oX = Double.valueOf(nextLine[5]);
 						double oY = Double.valueOf(nextLine[6]);
 						double dX = Double.valueOf(nextLine[9]);
 						double dY = Double.valueOf(nextLine[10]);
 
-						//project transformation
-					    p1.x = oX;
-					    p1.y = oY;
-						trans.transform(p1, p2);	
+						// project transformation
+						p1.x = oX;
+						p1.y = oY;
+						trans.transform(p1, p2);
 						dataitem[3] = String.valueOf(p2.x);
 						dataitem[4] = String.valueOf(p2.y);
-						
-						
-						 p1.x = dX;
-						 p1.y = dY;
-						 trans.transform(p1, p2);
-						
+
+						p1.x = dX;
+						p1.y = dY;
+						trans.transform(p1, p2);
+
 						dataitem[5] = String.valueOf(p2.x);
 						dataitem[6] = String.valueOf(p2.y);
 						writer.writeNext(dataitem);
-				}
+					} catch (Exception e) {
 
+					}
+				}
 				writer.close();
 			}
-		}
-	    
-	}
-	
-	
-	
 
+		}
+	}
 
 }
---
+++ src/data/processing/TemporalPartition.java
@@ -0,0 +1,119 @@
+package data.processing;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Locale;
+
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+
+//partition the data file into hour duration.
+public class TemporalPartition {
+
+	public static void main(String[] args) throws Exception {
+
+		String[] years = { "2011", "2012", "2013", "2014", "2015" };
+		String[] months = { "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12" };
+
+		DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.US);
+
+		// the start time of the dataset.
+		Calendar origin_date = Calendar.getInstance();
+		origin_date.setTime(formatter.parse("2009-01-01 00:00"));
+		long origin_time = origin_date.getTimeInMillis();
+
+		Calendar cal_departure = Calendar.getInstance();
+		Calendar cal_arrival = Calendar.getInstance();
+
+		for (int y = 0; y < years.length; y++) {
+			for (int m = 0; m < months.length; m++) {
+
+				HashMap<Integer, ArrayList<String[]>> data_segement = new HashMap<Integer, ArrayList<String[]>>();
+				String year = years[y];
+				String month = months[m];
+				String filename = "D:\\Data\\NY_Taxi_tripdata\\yellow_tripdata_" + year + "-" + month + ".csv";
+				System.out.println(filename);
+
+				CSVReader reader = new CSVReader(new FileReader(filename));
+				reader.readNext();
+				String[] nextLine;
+
+				while ((nextLine = reader.readNext()) != null) {
+
+					cal_departure.setTime(formatter.parse(nextLine[0]));
+					cal_arrival.setTime(formatter.parse(nextLine[1]));
+					int duration = (int) ((cal_arrival.getTimeInMillis() - cal_departure.getTimeInMillis()) / 60000);
+					int departure = (int) ((cal_departure.getTimeInMillis() - origin_time) / 3600000);
+
+					if (data_segement.get(departure) != null) {
+						ArrayList<String[]> data = data_segement.get(departure);
+						String[] data_item = new String[6];
+						data_item[0] = String.valueOf(duration);
+						data_item[1] = nextLine[2];
+						data_item[2] = nextLine[3];
+						data_item[3] = nextLine[4];
+						data_item[4] = nextLine[5];
+						data_item[5] = nextLine[6];
+						data.add(data_item);
+						data_segement.put(departure, data);
+					} else {
+						ArrayList<String[]> data = new ArrayList<String[]>();
+						String[] data_item = new String[6];
+						data_item[0] = String.valueOf(duration);
+						data_item[1] = nextLine[2];
+						data_item[2] = nextLine[3];
+						data_item[3] = nextLine[4];
+						data_item[4] = nextLine[5];
+						data_item[5] = nextLine[6];
+						data.add(data_item);
+						data_segement.put(departure, data);
+					}
+
+				}
+
+				for (Integer time : data_segement.keySet()) {
+					String outputfile = "D:\\Data\\NY_Taxi_tripdata2\\" + time + ".csv";
+					CSVWriter writer = new CSVWriter(new FileWriter(outputfile), ',', CSVWriter.NO_QUOTE_CHARACTER);
+					// feed in your array (or convert your data to an array)
+					String[] entries = new String[6];
+
+					entries[0] = "Trip_Duration";
+					entries[1] = "Trip_Distance";
+					entries[2] = "UTM_x1";
+					entries[3] = "UTM_y1";
+					entries[4] = "UTM_x2";
+					entries[5] = "UTM_y2";
+
+					writer.writeNext(entries);
+					ArrayList<String[]> data = data_segement.get(time);
+
+					for (int j = 0; j < data.size(); j++) {
+
+						try {
+							String[] dataitem = new String[6];
+							dataitem[0] = data.get(j)[0];
+							dataitem[1] = data.get(j)[1];
+							dataitem[2] = data.get(j)[2];
+							dataitem[3] = data.get(j)[3];
+							dataitem[4] = data.get(j)[4];
+							dataitem[5] = data.get(j)[5];
+							writer.writeNext(dataitem);
+						} catch (Exception e) {
+
+						}
+					}
+					writer.close();
+				}
+
+			}
+		}
+
+	}
+
+}
